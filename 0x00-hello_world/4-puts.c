fd2ce06dbf7038a57045aa2ba7100cfe1165f214
romisaa <romisaa.hamdy@yahoo.com>
2023-08-10 07:11:18 -0700

    name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Sucess)
+ *\
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
