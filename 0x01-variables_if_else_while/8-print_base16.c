7cbc76d1d5ed83806e3235ba998ecdca6d543f9a
romisaa <romisaa.hamdy@yahoo.com>
2023-08-11 15:40:28 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+char alphabet;
+for (x = '0'; x < '9' ; x++)
+{
+putchar(x);
+}
+for (alphabet = 'a'; alphabet <= 'f' ; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
