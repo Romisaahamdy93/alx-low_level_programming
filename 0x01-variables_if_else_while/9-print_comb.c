d4ca1d90937eccc2f4c1013704707fc2435babb1
romisaa <romisaa.hamdy@yahoo.com>
2023-08-11 16:08:50 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+int s;
+for (s = 0; s <= 9 ; s++)
+{
+if (s <= 9 && s > 0)
+{
+putchar(',');
+putchar(' ');
+}
+putchar((s % 10) + '0');
+}
+putchar('$')
+return (0);
+}
