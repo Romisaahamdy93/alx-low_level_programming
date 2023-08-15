3564fa8574905cb69c391d77756b25db994290f6
romisaa <romisaa.hamdy@yahoo.com>
2023-08-15 06:24:43 -0700

    alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,13 +1,12 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - check the code for Holberton School students.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0.
  */
-void print_alphabet(void);
+int main(void)
 {
-	return (write(1, &c, 1));
+	print_alphabet();
+	return (0);
 }
