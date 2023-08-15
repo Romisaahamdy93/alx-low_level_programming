f31ccca243b04455d0ba8d5c980330c6d7809d05
romisaa <romisaa.hamdy@yahoo.com>
2023-08-15 16:21:12 -0700

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,10 +6,10 @@
  */
 void times_table(void)
 {
-	int (x, y, z);
-	for (x = 0, x < 10; x++)
+	int x, y, z;
+	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 10, y++)
+		for (y = 0; y < 10; y++)
 		{
 			z = x * y;
 			if (y == 0)
