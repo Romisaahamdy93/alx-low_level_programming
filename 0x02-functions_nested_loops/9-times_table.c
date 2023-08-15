d880f17767a70e37356e22d6337d76b386a08e3d
romisaa <romisaa.hamdy@yahoo.com>
2023-08-15 16:33:25 -0700

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,35 +2,34 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  *
- * Return: 1 and prints + if n is greater than zero
  */
 void times_table(void)
 {
-	int x, y, z;
-	for (x = 0; x < 10; x++)
-	{
-		for (y = 0; y < 10; y++)
-		{
-			z = x * y;
-			if (y == 0)
-			{
-				_putchar(z + '0');
-			}
-			if (z < 10 && y != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-			else if (z >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((z / 10) + '0');
-				_putchar((z % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+int x, y, z;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 10; y++)
+{
+z = x * y;
+if (y == 0)
+{
+_putchar(z + '0');
+}
+if (z < 10 && y != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(z + '0');
+}
+else if (z >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((z / 10) + '0');
+_putchar((z % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
