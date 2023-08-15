57de381e73c9fd3d2f36e0d58eb8f1231d10a51c
romisaa <romisaa.hamdy@yahoo.com>
2023-08-15 08:00:51 -0700

    a;phabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+char (c);
+int (i);
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+}
+}
