8b9dd6e5e281e712303a951ee036c9332e3ff7e5
romisaa <romisaa.hamdy@yahoo.com>
2023-08-15 16:05:47 -0700

    24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,11 +2,10 @@
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  *
- * Return: 0
  */
 void jack_bauer(void)
 {
-	int(x, y);
+	int x, y;
 	for (x = 0, x < 24, x++)
 	{
 		for (y = 0, y < 60, y++)
