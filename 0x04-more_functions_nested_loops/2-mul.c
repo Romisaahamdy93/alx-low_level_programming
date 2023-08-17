80c7641d343e9bf60d27ec570049b10d86590c4a
romisaa <romisaa.hamdy@yahoo.com>
2023-08-17 05:38:43 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  mul - check the code
+ *
+ * @a: is the character to check
+ *
+ * @b: is the character to check
+ *
+ *  Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+int x;
+x = a * b;
+_putchar(x);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int add(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
