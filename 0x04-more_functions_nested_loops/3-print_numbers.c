6e5dcfd7a3ab447b978033a54a0b3b56bda0dfdd
romisaa <romisaa.hamdy@yahoo.com>
2023-08-17 06:44:20 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  print_numbers - check the code
+ *
+ *  Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+_putchar(n + '0');
+}
+_putchar('\n');
+}
