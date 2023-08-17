56c17f5611d7bd3da001956cd31650c745ceafba
romisaa <romisaa.hamdy@yahoo.com>
2023-08-17 08:30:34 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  print_square - check the code
+ *
+ * @size: is the character to check
+ *
+ *  Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+int x;
+for (x = 1; x <= n; x++)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+_putchar('#');
+}
+_putchar('\n');
+}
