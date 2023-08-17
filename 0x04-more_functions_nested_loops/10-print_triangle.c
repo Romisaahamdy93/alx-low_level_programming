79933d6e3c6525417c889fd6587d294a6bbed485
romisaa <romisaa.hamdy@yahoo.com>
2023-08-17 14:24:33 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,12 @@
 void print_triangle(int size)
 {
 int i, n;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
 for (i = 1; i <= size; i++)
 {
 for (n = i; n < size; n++)
@@ -17,11 +23,9 @@ _putchar(' ');
 }
 for (n = 1; n <= i; n++)
 {
-putchar('#');
+_putchar('#');
 }
-if (size <= 0)
-{
-putchar('\n');
+_putchar('\n');
 }
 }
 }
