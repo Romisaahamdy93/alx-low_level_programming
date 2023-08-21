341238076d27f5be6139244ff59a40ee0945a538
romisaa <romisaa.hamdy@yahoo.com>
2023-08-21 10:35:44 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - akes a pointer to an int
+ *
+ * @n: is the character to check
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
