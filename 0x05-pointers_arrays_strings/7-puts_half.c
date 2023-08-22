536c05a1ca4795253518ab0a83bd7c4f90ead58e
romisaa <romisaa.hamdy@yahoo.com>
2023-08-21 17:29:19 -0700

    strcp

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,22 @@
  */
 void puts_half(char *str)
 {
-int n;
+int n, i, j;
 for (n = 0; str[n] != '0'; n++)
 {
-n++;
-}
-n++;
-for (n /= 2; str[n] != '0' ; n++)
+if (n % 2 == 0)
 {
-_putchar(str[n]);
+for (i = n / 2; str[i] != '0' ; i++)
+{
+_putchar(str[i]);
+}
+else if (n % 2)
+{
+for (j = (n - 1) / 2; j < n - 1; j++)
+{
+_putchar(str[j + 1])
+}
+}
 }
 _putchar('\n');
 }
