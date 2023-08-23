4d7460227ca7b64f6576a875368b3d6e184a4b0a
romisaa <romisaa.hamdy@yahoo.com>
2023-08-23 13:18:48 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,4 +10,21 @@
  *
  * Return: dest
  */
-
+char *_strncat(char *dest, char *src, int n)
+{
+int a, b;
+a = 0;
+b = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+while (src[b] != '\0' && b < n)
+{
+dest[a] = src[b];
+b++;
+a++;
+}
+dest[a] = '\0';
+return (dest);
+}
