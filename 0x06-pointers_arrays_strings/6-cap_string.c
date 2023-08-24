93c41e0d0d18f8eaafb3497d03871d2e947907ab
romisaa <romisaa.hamdy@yahoo.com>
2023-08-24 06:22:52 -0700

    capstring

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: is thre string
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+int x, y;
+char sp[13] = {' ', '\t', '\n', ',', ';', '.',
+	'!', '?', '"', '(', ')', '{', '}'}
+for (x = 0; s[x] != '\0'; x++)
+{
+if (x == 0 && s[x] >= 'a' && s[x] <= 'z')
+{
+s[x] = s[x] - 32;
+}
+for (y = 0; y < 13; y++)
+{
+if (s[x] == s[y])
+{
+if (s[x + 1] >= 'a' && s[x + 1] <= 'z')
+{
+s[x + 1] = s[x + 1] - 32;
+}
+}
+}
+}
+return (s);
+}
