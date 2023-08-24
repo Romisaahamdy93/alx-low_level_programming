7e9db2aa4c7e14b6aa74577a6750ab03051ce499
romisaa <romisaa.hamdy@yahoo.com>
2023-08-24 06:44:56 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *leet -  encodes a string into 1337
+ * @s: is the string
+ * Return: s
+ */
+char *leet(char *s)
+{
+int x, j;
+char a[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T'
+	'l', 'L'};
+char b[10] = {'4', '4', '3', '3', '0', '0', '7', '7'
+	'1', '1'};
+for (x = 0; s[x] != '\0'; x++)
+{
+for (y = 0; y < 10; y++)
+{
+if (s[x] == a[y])
+{
+s[x] = b[y];
+}
+}
+}
+return (s);
+}
