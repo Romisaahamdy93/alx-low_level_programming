12c4ad1887702e140c154172fcd07ed467c62c32
romisaa <romisaa.hamdy@yahoo.com>
2023-08-28 16:02:43 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,9 +11,9 @@ char *_strpbrk(char *s, char *accept)
 int x, y;
 for (x = 0; s[x] != '\0'; x++)
 {
-for (y = 0; s[y] != '\0'; y++)
+for (y = 0; accept[y] != '\0'; y++)
 {
-if (s[x] == s[y])
+if (s[x] == accept[y])
 return (s);
 }
 s++;
