fced15231a980748c53970650b64aaa5dd7deb44
romisaa <romisaa.hamdy@yahoo.com>
2023-09-01 05:50:18 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,12 +13,11 @@ for (a = 0; haystack[a] != '\0'; a++)
 {
 for (b = 0; needle[b] != '\0'; b++)
 {
-if (haystack[a] != needle[a])
+if (haystack[a + b] != needle[b])
 break;
 }
-if (a != b)
-haystack++;
-else
-return (haystack);
+if (!needle[b])
+return (&haystack[a]);
 }
+return ('\0');
 }
