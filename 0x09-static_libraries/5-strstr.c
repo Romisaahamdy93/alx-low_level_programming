fac48a1d390600915152656108a2d61c6c340ad9
romisaa <romisaa.hamdy@yahoo.com>
2023-09-01 06:26:23 -0700

    libmy

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  *_strstr - finds the first occurrence of the substring
+ *  @haystack: string to check
+ *  @needle: substring to check
+ *  Return: pointer to the beginning of the located substring
+ *  or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int a, b;
+for (a = 0; haystack[a] != '\0'; a++)
+{
+for (b = 0; needle[b] != '\0'; b++)
+{
+if (haystack[a + b] != needle[b])
+break;
+}
+if (!needle[b])
+return (&haystack[a]);
+}
+return ('\0');
+}
