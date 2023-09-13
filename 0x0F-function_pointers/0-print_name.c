6addc5b71648a88af2cedaa21f7fe1a599d88e7f
romisaa <romisaa.hamdy@yahoo.com>
2023-09-13 12:45:22 -0700

    print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - prints a name
+ * @name: is a character to check
+ * @f: is a function to pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL || f != NULL)
+f(name);
+}
