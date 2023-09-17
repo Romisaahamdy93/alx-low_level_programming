b8edc0eeb464f70234d8cdc8518d1a15af2f1628
Romisaa <romisaa.hamdy@yahoo.com>
2023-09-17 08:26:20 -0700

    printall

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,78 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_char - Prints a character 
+ * @separator: The separator
+ * @args: arguments
+ */
+void print_char(char *separator, va_list args)
+{
+printf("%s%c", separator, va_arg(args, int));
+}
+/**
+ * print_float - Prints a float
+ * @separator: The separator
+ * @args: arguments
+ */
+void print_float(char *separator, va_list args)
+{
+printf("%s%f", separator, va_arg(args, double));
+}
+/**
+ * print_int - Prints a integer
+ * @separator: The separator
+ * @args: arguments
+ */
+void print_int(char *separator, va_list args)
+{
+printf("%s%i", separator, va_arg(args, int));
+}
+/**
+ * print_str - Prints a string
+ * @separator: The separator
+ * @args: arguments
+ */
+void print_str(char *separator, va_list args)
+{
+char *string;
+string = va_arg(args, char *);
+if (string == NULL)
+printf("%s%s", separator, "(nil");
+printf("%s%s", separator, string);
+}
+/**
+ * print_all -  prints anything
+ * @format:  is a list of types of arguments
+ */
+void print_all(const char * const format, ...)
+{
+p_t types[] = {
+{"c", print_char},
+{"i", print_int},
+{"f", print_float},
+{"s", print_str},
+{NULL, NULL}
+};
+int a = 0;
+int b = 0;
+char *separator = "";
+va_list args;
+va_start(args, format);
+while (format != NULL && format[a])
+{
+b = 0;
+while (b < 4)
+{
+if (format[a] == *types[b].print)
+{
+types[b].f(separator, args);
+separator = ", ";
+}
+b++;
+}
+a++;
+}
+va_end(args);
+printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,15 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct types - Struct format_types
+ * @print: The conversion specifier
+ * @f: The function pointer
+ */
+typedef struct types
+{
+	char *print;
+	void (*f)(char *separator, va_list args);
+} p_t;
 
 #endif
