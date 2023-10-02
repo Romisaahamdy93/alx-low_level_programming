3749d5d11962da411d55a0fcb11a3fb8a225c182
Romisaa <romisaa.hamdy@yahoo.com>
2023-10-02 14:52:34 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int op, wr;
 int len;
 if (filename == NULL)
 return (-1);
-op = open(filename, O_WRONLY, | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (op == -1)
 return (-1);
 if (text_content != NULL)
