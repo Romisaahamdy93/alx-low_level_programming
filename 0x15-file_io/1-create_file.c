413436ff6bfdd6c4ba613d08a1e708897c6e775c
Romisaa <romisaa.hamdy@yahoo.com>
2023-10-02 14:59:21 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,10 +8,10 @@
 int create_file(const char *filename, char *text_content)
 {
 int op, wr = 0;
-int len;
+int len = 0;
 if (filename == NULL)
 return (-1);
-op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (op == -1)
 return (-1);
 if (text_content != NULL)
