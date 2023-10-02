5a60aa251519bbc9bb7a81a554b5f6a0478e7d7e
Romisaa <romisaa.hamdy@yahoo.com>
2023-10-02 14:51:16 -0700

    create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ * @filename: is the file name
+ * @text_content: is a string
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int op, wr;
+int len;
+if (filename == NULL)
+return (-1);
+op = open(filename, O_WRONLY, | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (op == -1)
+return (-1);
+if (text_content != NULL)
+{
+while (text_content[len] != '\0')
+len++;
+}
+wr = write(op, text_content, len);
+if (wr == -1 || wr != len)
+return (-1);
+close(op);
+return (1);
+}
