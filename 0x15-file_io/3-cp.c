199e8d04dfd5455bb9fe93486c452ad926a1eb36
Romisaa <romisaa.hamdy@yahoo.com>
2023-10-04 11:37:03 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,19 +11,25 @@ int from_op, to_op, re;
 char buff[BUFSIZ];
 
 if (argc != 3)
+{
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
+}
 from_op = open(argv[1], O_RDONLY);
 if (from_op == -1)
+{
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
+}
 to_op = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (to_op == -1)
+{
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
+}
 while ((re = read(from_op, buff, BUFSIZ)) > 0)
-{
 if (write(to_op, buff, re) != re)
+{
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
