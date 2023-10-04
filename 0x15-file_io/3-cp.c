95ed915f5d8dfa1c7333ab5e9624b69de23610cd
Romisaa <romisaa.hamdy@yahoo.com>
2023-10-04 11:25:00 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,8 @@
 int main(int argc, char *argv[])
 {
 int from_op, to_op, re;
-char buf[BUFSIZ];
+char buff[BUFSIZ];
+
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -23,13 +24,13 @@ exit(98);
 to_op = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (to_op == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
-while ((re = read(from_op, buf, BUFSIZ)) > 0)
+while ((re = read(_from, buff, BUFSIZ)) > 0)
 {
-if (write(to_op, buf, re) != re)
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+if (write(to_op, buff, re) != re)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 if (re == -1)
@@ -37,14 +38,12 @@ if (re == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-from_op = close(from_op);
-to_op = close(to_op);
-if (from_op == -1)
+if (close(from_op) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close %d\n", from_op);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_op);
 exit(100);
 }
-if (to_op == -1)
+if (close(to_op) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close %d\n", to_op);
 exit(100);
