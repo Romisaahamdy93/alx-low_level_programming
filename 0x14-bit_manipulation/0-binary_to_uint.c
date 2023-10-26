ca6af2c5bb2769c9de3a9026467a36c80f813cb5
Romisaa <romisaa.hamdy@yahoo.com>
2023-10-26 06:20:59 -0700

    btou

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,4 +5,16 @@
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
-
+{
+int x;
+unsigned int num = 0;
+if (b == NULL)
+return (0);
+for (x = 0; b[x]; x++)
+{
+if (b[x] != '0' && b[x] != '1')
+return (0);
+num = num * 2 + (b[x] - '0');
+}
+return (num);
+}
