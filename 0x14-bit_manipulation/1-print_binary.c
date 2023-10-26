5d003b78f4267664d08eef38a9afd83320f55ffa
Romisaa <romisaa.hamdy@yahoo.com>
2023-10-26 06:27:30 -0700

    print

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number of binary to printed
+ */
+void print_binary(unsigned long int n)
+{
+int p = 0;
+int b = sizeof(n) * 8 - 1;
+if (n == 0)
+_putchar('0');
+while (b >= 0)
+{
+if (n > b & 1)
+{
+_putchar('1');
+p++;
+}
+else if (p)
+_putchar('0');
+b--;
+}
+}
