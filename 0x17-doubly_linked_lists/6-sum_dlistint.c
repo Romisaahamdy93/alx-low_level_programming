395bbc2015c9373799ffb8f28cad16d7b09090dd
Romisaa <romisaa.hamdy@yahoo.com>
2023-12-07 13:09:44 -0800

    sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * sum_dlistint - eturns the sum of all the data (n) of linked list
+ * @head: is the head of list
+ * Return: 0 if the list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int sum;
+for (sum = 0; head != NULL; head = head->next)
+sum = sum + head->n
+return (sum);
+}
