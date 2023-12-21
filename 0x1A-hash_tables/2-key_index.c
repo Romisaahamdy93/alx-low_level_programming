8be2ed9ad1e7f557c62f54c850ea312cf06095d1
romisaa <romisaa.hamdy@yahoo.com>
2023-12-21 15:41:42 -0800

    edit

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,9 +7,9 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-unsigned long int hash_value, index;
+unsigned long int index;
 if (key == NULL || size == 0)
 return (0);
-hash_value = hash_djb2(key) % size;
+index = hash_djb2(key) % size;
 return (index);
 }
